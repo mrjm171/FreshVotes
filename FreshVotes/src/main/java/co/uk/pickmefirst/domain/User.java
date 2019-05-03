130c1718ae4fb1b43fb9cb07e8ceaa3891b38961
mrjm171 <markrjmclaggan@hotmail.co.uk>
2019-05-03 21:58:10 +0100

    Creation of entities

---
+++ FreshVotes/src/main/java/co/uk/pickmefirst/domain/User.java
@@ -0,0 +1,44 @@
+package co.uk.pickmefirst.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User 
+{
+	private Long id;
+	private String username;
+	private String passowrd;
+	private String name;
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassowrd() {
+		return passowrd;
+	}
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
