605560548a74a8ba2203d5bd3f82d9db0bb7d13a
mrjm171 <markrjmclaggan@hotmail.co.uk>
2019-05-06 16:47:25 +0100

    Creating a custom login page

--- FreshVotes/src/main/java/co/uk/pickmefirst/web/DashboardController.java
+++ FreshVotes/src/main/java/co/uk/pickmefirst/web/DashboardController.java
@@ -1,4 +1,4 @@
-package co.uk.pickmefirst;
+package co.uk.pickmefirst.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DashboardController 
 {
-	@RequestMapping (value="/", method=RequestMethod.GET)
+	@GetMapping (value="/")
 		public String rootView() {
 			return "index";
 	}
