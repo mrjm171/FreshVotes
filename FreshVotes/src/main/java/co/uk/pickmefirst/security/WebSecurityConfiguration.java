842d2da753e7a43a327ac54388090209d31bccfd
mrjm171 <markrjmclaggan@hotmail.co.uk>
2019-05-07 22:44:17 +0100

    Implementing user details

--- FreshVotes/src/main/java/co/uk/pickmefirst/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/co/uk/pickmefirst/security/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package co.uk.pickmefirst.security;import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
