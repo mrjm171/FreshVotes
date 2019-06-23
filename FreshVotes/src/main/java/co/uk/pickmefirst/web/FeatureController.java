2d461d8d9bba1b490099d0503530bd57e314708d
mrjm171 <markrjmclaggan@hotmail.co.uk>
2019-06-23 21:52:05 +0100

    adding CRUD to feature request

--- FreshVotes/src/main/java/co/uk/pickmefirst/web/FeatureController.java
+++ FreshVotes/src/main/java/co/uk/pickmefirst/web/FeatureController.java
@@ -1,11 +1,18 @@
 package co.uk.pickmefirst.web;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.uk.pickmefirst.domain.Feature;
 import co.uk.pickmefirst.service.FeatureService;
 
+@Controller
 @RequestMapping("/products/{productId}/features")
 public class FeatureController {
   
@@ -14,8 +21,28 @@ public class FeatureController {
   
   @PostMapping("") // this maps to -> '/products/{productId}/features'
   public String createFeature(@PathVariable Long productId) {
-    featureService.createFeature(productId);
+    Feature feature = featureService.createFeature(productId);
+    
+    return "redirect:/products/"+productId+"/features/"+feature.getId();
+    
+  }
+  
+  @GetMapping("{featureId}")
+  public String getFeature(ModelMap model, @PathVariable Long productId, @PathVariable Long featureId) {
+    Optional<Feature> featureOpt = featureService.findById(featureId);
+    if (featureOpt.isPresent()) {
+      model.put("feature", featureOpt.get());
+    }
+    // TODO: handle the situation where we can't find a feature by the featureId
     
     return "feature";
   }
+  
+  @PostMapping("{featureId}")
+  public String updateFeature (Feature feature, @PathVariable Long productId, @PathVariable Long featureId) {
+    feature = featureService.save(feature);
+    
+    return "redirect:/products/"+productId+"/features/"+feature.getId();
+  }
+  
 }
